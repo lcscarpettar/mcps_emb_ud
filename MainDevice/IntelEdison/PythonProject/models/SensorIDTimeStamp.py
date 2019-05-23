d8b86c30f2274a3abc434046a88f043ad31e046d
Jose Manuel Vargas <jomvargasm@outlook.com>
2019-05-23 18:11:54 -0500

    Se crea clase de Python 2.7 para gestionar IDs de los sensores y sincronizacion de timestamp con el server

---
+++ MainDevice/IntelEdison/PythonProject/models/SensorIDTimeStamp.py
@@ -0,0 +1,57 @@
+from uuid import getnode
+from datetime import datetime
+import requests
+import json
+import time
+import threading
+
+
+class SensorIDTimeStamp(threading.Thread):    
+    def __init__(self):
+        super().__init__()
+        self.server_id_url = "https://mcpsapi.azurewebsites.net/mcpsapi/Parametro"
+        self.obj_temp_name = "obj_temp_" + str(getnode())
+        self.amb_temp_name = "amb_temp_" + str(getnode())
+        self.obj_temp_id = -1
+        self.amb_temp_id = -1
+        self.server_timestamp = datetime.now().timestamp()
+        self.last_time = time.time()
+        self._getServerID()
+        self.bRunning = True
+        self.start()
+
+    def getServerTimeStamp(self):
+        diff_time = time.time() - self.last_time
+        return self.server_timestamp + diff_time
+
+    def stop(self):
+        self.bRunning = False
+
+    def _getServerID(self):
+        r = self._getServerTimeStamp(self.obj_temp_name)
+        if r and 'SensorID' in r:
+            self.obj_temp_id = int(r['SensorID'])
+        r = self._getServerTimeStamp(self.amb_temp_name)
+        if r and 'SensorID' in r:
+            self.amb_temp_id = int(r['SensorID'])
+
+    def _getServerTimeStamp(self, sensor_name):
+        try:
+            response = requests.get(self.server_id_url, params={'codigoSensor': sensor_name})
+            r = json.loads(response.content)
+            if 'TimeStamp' in r:
+                self.server_timestamp = float(r['TimeStamp'])
+                self.last_time = time.time()
+            return r
+        except Exception as ex:
+            print(ex)
+            return dict()
+
+    def run(self):
+        while self.bRunning:
+            self._getServerTimeStamp(self.obj_temp_name)
+            for i in range(60):
+                time.sleep(10)
+                if not self.bRunning:
+                    break
+
